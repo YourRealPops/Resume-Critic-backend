fb114dd2b28b09c8b5f82067de57b7b8daf64c41
YourRealPops <iamford16.af@gmail.com>
2026-02-18 14:00:41 +0100

    fixing render issues

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://resumecritic.netlify.app', 
+  ],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
