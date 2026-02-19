025a98e9b42a98a14440ba8c329ff94089cc6707
YourRealPops <iamford16.af@gmail.com>
2026-02-19 06:59:15 +0100

    changed api model

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel(
-  { model: "gemini-2.5-flash" },
+  { model: "gemini-2.0-flash" },
   { apiVersion: 'v1' }
 );
 
